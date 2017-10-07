2d2ac19096cfd3afe84ef6b69d3ec913ec36eec0
Myra Lukens <myralukens@gmail.com>
2017-10-07 16:35:03 -0700

    everything except copying is working

--- generate.js
+++ generate.js
@@ -1,29 +1,3 @@
-const explicit_words_file = "explicit.txt"
-const grated_words_file = "grated.txt"
-const end_punctuation_file = "endpunctuation.txt"
-const middle_punctuation_file = "middlepunctuation.txt"
-
-var result = generate(2, true);
-console.log(result);
-
-function generate(paragraphs, explicit) {
-   var result = generateSentence(explicit);
-
-   for (a = 0; a < paragraphs; a++) {
-      // 4 - 6 sentences per paragraph
-      var numSentences = Math.floor(Math.random() * 3) + 4;
-      console.log(numSentences);
-      for (b = 1; b < numSentences; b++) {
-         result += (" " + generateSentence(explicit));
-      }
-      if (a < (paragraphs - 1)) {
-         result += "\n\n";
-      }
-   }
-
-   return result;
-}
-
 // sentence is between 5 - 10 words
 function generateSentence(explicit) {
    // randly generate # of words
@@ -47,29 +21,42 @@ function generateSentence(explicit) {
    return sentence;
 }
 
-function generateWord(explicit) {
+function generateWord(isExplicit) {
    // if explicit mode is on, 25% chance word generated will be explicit
    var odds = Math.floor(Math.random() * 10);
-   var filename = grated_words_file;
 
    // explicit word
-   if (explicit && odds < 3) {
-      filename = explicit_words_file;
+   if (isExplicit && odds < 3) {
+      var array = explicit().split("\n");
+      return array[Math.floor(Math.random()*array.length)];
    } 
 
-   return getRandomWordFromFile(filename);
+   var array = grated().split("\n");
+   return array[Math.floor(Math.random()*array.length)];
 }
 
 function generateMiddleSentencePunctuation() {
-   return getRandomWordFromFile(middle_punctuation_file);
+   var array = middlepunctuation().split("\n");
+   return array[Math.floor(Math.random()*array.length)];
 }
 
 function generateEndPunctuation() {
-   return getRandomWordFromFile(end_punctuation_file);
+   var array = endpunctuation().split("\n");
+   return array[Math.floor(Math.random()*array.length)];
 }
 
-function getRandomWordFromFile(filename) {
-   var fs = require('fs');
-   var array = fs.readFileSync(filename).toString().split("\n");
-   return array[Math.floor(Math.random()*array.length)];
+function grated() {
+   return "Blue Bottle\nWeWork\nHayes Valley\nAirbnb\nmezcal\nWarby Parker\nSalesforce Tower\nbottomless mimosas\nLa Croix\nUber bros\ndog turd\nmission bars\nthe rent\nchihuahua mix\nstartup\ncoworking\nnetworking\ncold brew\nventure capital\nVC\nseed funding\nyoga\nSoulcycle\nBART\nMUNI\ndogwalk\nubered\nlyft\nuber\nbrainstorm\nfundraise\ncreate\nCaltrain\nhike\nbike ride\nPAC Heights\nThe Marina\nSOMA\nFiDi\nDivis\nAmazon Fresh\nAmazon Prime\nInstacart\nKarl the fog\nPatagucci\nPatagonia\nbay area\nbae area\nlit\nchill\nScoot\nscooter\nboosted board\ngentrification\ncorporate shuttle\nGoogle bus\noverthrowing technology companies\nnah\ntryna\nyee\nAlcatraz\nchic\nDolores Park\nfleet week\ndog\npurebred dog\nhypoallergenic dog\nSunday brunch\nviews\nelectric bike\nNob Hill\nNorth Beach\nbridge\nviews\nhippie\nmaternal heights\npublic transportation\nmusic festival\nOutside Lands\ntraffic\nwaiting in lines\nlines\nbarista\ncoffee\nthe 415\nno parking\nparking\nexpensive\nrent\nBurning Man\nthe Giants\nAT&T Park\nOracle Arena\nOakland peasants\nfake service animals\nyoga pants\ntop 1%\n1% of 1%\nlesbian cat sweater\nMission hipsters\nhipsters\navant garde\nartists\nartistic\ntech\ntechnology\nwhite people\ndiversity\nequality\nwomen's rights\nfeminism\nfeminists\nwhite, heterosexual men\nSpanish\nculture\nFerry Building\ntourists\nthe city\ngarden\nmuseums\nnight life at museums\nLyft";
+}
+
+function explicit() {
+   return "Mark Zuckerberg's dick\nfuck\ngod damn\nshit\nmother effing\nhella\ndick\nmofo\nweed at Dolores Park\nday drinking\ngay culture\nFolsom Street Fair\nnaked homeless people\nso many lesbians\nalcohol\ndrugs\nhangover\nrum and mango juice\nasshole\nMarc Benioff's 9-inch dick\nJack Dorsey's ass";
+}
+
+function middlepunctuation() {
+   return ",\n...\n;\n:";
+}
+
+function endpunctuation() {
+   return "!\n.\n...\n?";
 }
